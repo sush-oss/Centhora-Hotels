edbe0bdfde97081f18ee47953832ed3cf90a19de
Sadeesha-Dias <sddias19981230@gmail.com>
2023-04-03 23:01:46 +0530

    Created a service for authticate user's credentials.

---
+++ InternalServices/CenthoraAuth/CenthoraAuth.cs
@@ -0,0 +1,26 @@
+﻿using Centhora_Hotels.DB_Context;
+using Centhora_Hotels.Models.DTO;
+
+namespace Centhora_Hotels.InternalServices.CenthoraAuth
+{
+    public class CenthoraAuth : ICenthoraAuth
+    {
+        private readonly CenthoraDbContext _context;
+
+        public CenthoraAuth(CenthoraDbContext context)
+        {
+            _context = context;
+        }
+        public CenthoraAuthDto AuthenticateUser(string username, string password)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.UserName == username && u.UserPassword == password);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new CenthoraAuthDto { UserName = username, UserPassword = password };
+        }
+    }
+}
---
+++ InternalServices/CenthoraAuth/ICenthoraAuth.cs
@@ -0,0 +1,9 @@
+﻿using Centhora_Hotels.Models.DTO;
+
+namespace Centhora_Hotels.InternalServices.CenthoraAuth
+{
+    public interface ICenthoraAuth
+    {
+        CenthoraAuthDto AuthenticateUser(string username, string password);
+    }
+}
