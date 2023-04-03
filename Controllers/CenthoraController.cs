5f57ef7d407fb394d55038561177aadc286969e5
Sadeesha-Dias <sddias19981230@gmail.com>
2023-04-03 23:00:40 +0530

    Created a login api endpoint and added JWT authentication token generation.

---
+++ Controllers/CenthoraController.cs
@@ -0,0 +1,63 @@
+﻿using Centhora_Hotels.InternalServices.CenthoraAuth;
+using Centhora_Hotels.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Centhora_Hotels.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class CenthoraController : ControllerBase
+    {
+        private readonly ICenthoraAuth centhoraAuth;
+        private readonly IConfiguration configuration;
+
+        public CenthoraController(ICenthoraAuth _auth, IConfiguration _configuration)
+        {
+            centhoraAuth = _auth;
+            configuration = _configuration;
+        }
+
+        // Login
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("api/[controller]/centhoralogin")]
+        public ActionResult CenthoraUserLogin(CenthoraAuthDto authDto)
+        {
+            var logingUser = centhoraAuth.AuthenticateUser(authDto.UserName, authDto.UserPassword);
+            if (logingUser == null)
+            {
+                return BadRequest(new { message = "Invalid username or password" });
+            }
+
+            // Generate a JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(configuration["JWT:Key"]);
+            var issure = configuration["JWT:Issure"];
+            var audience = configuration["JWT:Audience"];
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, logingUser.UserName)
+                }),
+                Issuer = issure,
+                Audience = audience,
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new {Token =  tokenString});
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 using Centhora_Hotels.DB_Context;
 using Centhora_Hotels.InternalServices.Calculate_Room_Price;
+using Centhora_Hotels.InternalServices.CenthoraAuth;
 using Centhora_Hotels.InternalServices.Upload_Image_To_AWS_S3;
 using Centhora_Hotels.Repository.Interface;
 using Centhora_Hotels.Repository.Repo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +27,7 @@ builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 // Injecting the internal services
 builder.Services.AddScoped<ICalculateRoomPrices, CalculateRoomPrices>();
 builder.Services.AddScoped<IUploadImage, UploadImage>();
+builder.Services.AddScoped<ICenthoraAuth,  CenthoraAuth>();
 
 // Injecting and configuring AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -30,6 +35,26 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Injecting the ILogger for loggings
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
+
+// JWT Autentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JWT:Key"])),
+        ValidIssuer = builder.Configuration["JWT:Issure"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+    };
+});
+
+// JWT Authorization
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -40,6 +65,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Use authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
