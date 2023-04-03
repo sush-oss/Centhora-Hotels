0b5274e5fdc268d6655426d9633d0ecad0810522
Sadeesha-Dias <sddias19981230@gmail.com>
2023-04-03 23:02:31 +0530

    Created a DTO for login

---
+++ Models/DTO/CenthoraAuthDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Centhora_Hotels.Models.DTO
+{
+    public class CenthoraAuthDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
